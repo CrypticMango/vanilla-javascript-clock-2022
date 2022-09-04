e4be5a864f860c589ab62910742ccb7af3af1c53
Lisa Allen <79679706+CrypticMango@users.noreply.github.com>
2022-09-04 12:31:18 -0600

    added arrays of days and months
    -if else function to display the day
    -declared variables
    -changed spans to p in html

--- app.js
+++ app.js
@@ -1,4 +1,56 @@
 const today = new Date();
 const time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+const month = today.getMonth();
+const day = today.getDay();
+const year = today.getFullYear();
+
+//displays the current time
+
+let currentTime = document.getElementById("current-time");
+currentTime.innerHTML = time;
+
+//displays the day of the week
+
+const daysOfWeek = ['Sunday','Monday','Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+
+function displayDay() {
+    if (day == 0) {
+    return daysOfWeek[0];
+   } else if (day == 1) {
+    return daysOfWeek[1];
+   } else if (day == 2) {
+    return daysOfWeek[2];
+   } else if (day == 3) {
+    return daysOfWeek[3];
+   } else if (day == 4) {
+    return daysOfWeek[4];
+   } else if (day == 5) {
+    return daysOfWeek[5];
+   } else { return daysOfWeek[6]}
+}
+
+let currentDate = document.getElementById("current-date");
+currentDate.innerHTML = displayDay();
+//displays the month of the year
+
+const monthArr = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+
+function displayMonth() {
+    if (month == 8) {
+        return monthArr[8];
+    }
+}
+
+
+
+console.log(displayMonth());
+console.log(displayDay());
 
 console.log(time);
+console.log(today);
+console.log(day);
+console.log(month);
+console.log(year);
+
+let todaysTime = document.getElementById("current-time").innerText;
+
