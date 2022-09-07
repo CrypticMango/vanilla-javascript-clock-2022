25e2b49da05654043b4905174e750b6b5f84f543
Lisa Allen <79679706+CrypticMango@users.noreply.github.com>
2022-09-06 18:24:16 -0600

    updated readability

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const today = new Date();
-const time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+const time =
+	today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
 const month = today.getMonth();
 const day = today.getDay();
 const calendarDay = today.getDate();
@@ -14,39 +15,67 @@ currentTime.innerHTML = time;
 
 //displays the day of the week
 
-const daysOfWeek = ['Sunday','Monday','Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+const daysOfWeek = [
+	"Sunday",
+	"Monday",
+	"Tuesday",
+	"Wednesday",
+	"Thursday",
+	"Friday",
+	"Saturday",
+];
 
 function displayDay() {
-    if (day == 0) {
-    return daysOfWeek[0];
-   } else if (day == 1) {
-    return daysOfWeek[1];
-   } else if (day == 2) {
-    return daysOfWeek[2];
-   } else if (day == 3) {
-    return daysOfWeek[3];
-   } else if (day == 4) {
-    return daysOfWeek[4];
-   } else if (day == 5) {
-    return daysOfWeek[5];
-   } else { return daysOfWeek[6]}
+	if (day == 0) {
+		return daysOfWeek[0];
+	} else if (day == 1) {
+		return daysOfWeek[1];
+	} else if (day == 2) {
+		return daysOfWeek[2];
+	} else if (day == 3) {
+		return daysOfWeek[3];
+	} else if (day == 4) {
+		return daysOfWeek[4];
+	} else if (day == 5) {
+		return daysOfWeek[5];
+	} else {
+		return daysOfWeek[6];
+	}
 }
-
 
 //displays the month of the year
 
-const monthArr = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+const monthArr = [
+	"Jan",
+	"Feb",
+	"Mar",
+	"Apr",
+	"May",
+	"Jun",
+	"Jul",
+	"Aug",
+	"Sep",
+	"Oct",
+	"Nov",
+	"Dec",
+];
 
 function displayMonth() {
-    if (month == 8) {
-        return monthArr[8];
-    }
+	if (month == 8) {
+		return monthArr[8];
+	}
 }
 
 let currentDate = document.getElementById("current-date");
-currentDate.innerHTML = displayDay() + " " + displayMonth() + " " + calendarDay + " " + year;
-
-
+currentDate.innerHTML =
+	displayDay() +
+	"," +
+	" " +
+	displayMonth() +
+	" " +
+	calendarDay +
+	" " +
+	year;
 
 console.log(time);
 console.log(today);
